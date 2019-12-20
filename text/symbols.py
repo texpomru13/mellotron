baa388689479c03f9081daab927fc5feac1b0fb1
texpomru13 <texpomru13@gmail.com>
2019-12-20 19:31:37 +0500

    Update symbols.py

--- text/symbols.py
+++ text/symbols.py
@@ -11,7 +11,7 @@ _math = '#%&*+-/[]()'
 _special = '_@©°½—₩€$'
 _accented = 'áçéêëñöøćž'
 _numbers = '0123456789'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+_letters = 'ЙЦУКЕНГШЩЗХЪЁФЫВАПРОЛДЖЭЯЧСМИТЬБЮйцукенгшщзхъёфывапролджэячсмитьбю'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as
 # uppercase letters):
